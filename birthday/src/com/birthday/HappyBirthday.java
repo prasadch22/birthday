401f003bddde2ec33532c24abbdc36841d0f1aff
Prasad <Prasad@DESKTOP-UH8FRNB>
2018-12-04 12:12:20 +0530

    First commit

---
+++ birthday/src/com/birthday/HappyBirthday.java
@@ -0,0 +1,24 @@
+package com.birthday;
+
+public class HappyBirthday {
+	
+	
+	public static void main(String[] args) {
+		
+		int count ;
+		count=1;
+		
+		while (count<=100)
+		{
+			System.out.println("happy birthday...!");
+			count++;	
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+}
